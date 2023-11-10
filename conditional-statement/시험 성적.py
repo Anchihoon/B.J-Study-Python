c2b81b78c470ed425ca2aa0348ebf718b4336189
Anchihoon <qach88@naver.com>
2023-11-10 21:56:50 +0900

    시험 성적
    BAEKJOON ONLINEJUDGE 9498

---
+++ conditional-statement/시험 성적.py
@@ -0,0 +1,12 @@
+g = input()
+g = int(g)
+if g >= 90 and g <= 100 :
+    print('A')
+elif g >= 80 and g <= 89 :
+    print('B')
+elif g >= 70 and g <= 79 :
+    print('C')
+elif g >= 60 and g <= 69 :
+    print('D')
+else :
+    print('F')    
